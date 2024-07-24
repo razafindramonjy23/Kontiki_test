ec92b66d368c4c0461db09b699d04a05823b7e9f
Red_Rabbit <159623002+razafindramonjy23@users.noreply.github.com>
2024-07-24 11:03:36 +0100

    Boutton du envoyer message

--- Kontiki/front/src/components/pages/Contact/Contact.jsx
+++ Kontiki/front/src/components/pages/Contact/Contact.jsx
@@ -27,40 +27,46 @@ function Contact() {
             <div className="left">
               <div className="form-wrapper">
                 <div className="contact-heading">
-                  <h1>Travaillons ensembles<span>.</span></h1>
+                  <h1>Travaillons ensembles.</h1>
                   <p className="texte">Ou Contactez-nous via : <a href="malto:emailKTK@gmail.com">emailKTK@gmail.com</a></p>
                 </div>
 
                 <form action="#" className="contact-form" method='post'>
                   <div className="input-wrap">
-                    <input type="text" className="contact-input" required name='nom' autoComplete='off'/>
+                    <input type="text" className="contact-input" required name='nom' autoComplete='on'/>
                     <label>Nom</label>
-                    <img width="35" height="35" src="https://img.icons8.com/pastel-glyph/64/000000/name.png" alt="name"  className='icon'/>
+                    <img className='icon' width="35" height="35" src="https://img.icons8.com/pulsar-gradient/48/000000/user.png" alt="user"/>
                   </div>
 
                   <div className="input-wrap">
                     <input type="text" className="contact-input" required name='prenom' autoComplete='off'/>
                     <label>Prenom</label>
-                    <img width="35" height="35" src="https://img.icons8.com/pastel-glyph/64/000000/name.png" alt="name" className='icon'/>
+                    <img className='icon' width="35" height="35" src="https://img.icons8.com/pulsar-gradient/48/000000/user.png" alt="user"/>
                   </div>
 
                   <div className="input-wrap w-100">
                     <input type="email" className="contact-input" required name='Email' autoComplete='off'/>
                     <label>Email</label>
-                    <img width="35" height="35" src="https://img.icons8.com/ios/50/circled-envelope.png" alt="circled-envelope" className='icon'/>
+                    <img className='icon' width="35" height="35" src="https://img.icons8.com/fluency/48/000000/circled-envelope.png" alt="circled-envelope"/>
                   </div>
 
                   <div className="input-wrap textarea w-100">
                     <textarea name="Message" autoComplete='off' id="" className='contact-input' required></textarea>
                     <label>Message</label>
-                    <img width="35" height="35" src="https://img.icons8.com/ios/50/chat-message--v1.png" alt="chat-message--v1" className='icon'/>
+                    <img className='icon' width="35" height="35" src="https://img.icons8.com/fluency/48/000000/chat-message.png" alt="chat-message"/>
                   </div>
 
                   <div className="contact-buttons w-100">
-                    <button className="btn upload">
-                      Envoyer message
+                    <button >
+                      <div className="wrapper-1">
+                        <div className="wrapper">
+                        <img className='iconSend' width="48" height="48" src="https://img.icons8.com/fluency/48/000000/circled-right-2.png" alt="circled-right-2"/>
+                        </div>
+                      </div>
+                      <span>Envoyer</span>
                     </button>
                   </div>
+                  
                 </form>
               </div>
             </div>
