fcd53d68e9fb36f3858a95dfb33db789038843fb
Red_Rabbit <159623002+razafindramonjy23@users.noreply.github.com>
2024-07-30 17:03:08 +0100

    Couleurs de train test

--- Kontiki/front/src/components/pages/Services/Services.jsx
+++ Kontiki/front/src/components/pages/Services/Services.jsx
@@ -4,7 +4,29 @@ import './Services.css'
 function Services() {
   return (
     <div className='container'>
-      <h1>Services</h1>
+      <h1>Nos activités principales</h1>
+
+      <div className="leads">
+        <h1>Campagnes Emailing</h1>
+        <p>Collectez des prospects qualifiés et générez de nouveaux clients.</p>
+      </div>
+
+      <div className="leads">
+        <h1>Qualification des prospects</h1>
+        <p>Nous qualifions les contacts pour cibler au plus juste les prospects de nos clients.</p>
+      </div>
+
+      <p>Faites-nous confiance</p>
+      <h2>DEVELOPPONS ENSEMBLE VOTRE AVENIR DIGITAL</h2>
+
+      <div className="forces">
+        <h1>NOS FORCES</h1>
+        <p>Equipe jeune, dynamique et compétente
+          Large éventail de métiers intéressants
+          Ambiance conviviale et professionnelle
+          En pleine croissance</p>
+      </div>
+
     </div>
   )
 }
