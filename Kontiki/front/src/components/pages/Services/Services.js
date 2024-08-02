7e399f962d7e1f9dcd381add84f2f39f37aef839
Red_Rabbit <159623002+razafindramonjy23@users.noreply.github.com>
2024-08-02 17:49:33 +0100

    Servics terminee

--- Kontiki/front/src/components/pages/Services/Services.js
+++ Kontiki/front/src/components/pages/Services/Services.js
@@ -1,9 +1,61 @@
-const slider = document.querySelector('.slider');
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning') 
 
-function activate(e) {
-  const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+let timeRunning = 3000 
+let timeAutoNext = 7000
+
+nextBtn.onclick = function(){
+    showSlider('next')
 }
 
-document.addEventListener('click',activate,false);
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+let runTimeOut 
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight 
+    runningTime.style.animation = null 
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+
+    clearTimeout(runTimeOut)
+
+    runTimeOut = setTimeout( () => {
+        carousel.classList.remove('next')
+        carousel.classList.remove('prev')
+    }, timeRunning)
+
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation() 
+}
+
+// Start the initial animation 
+resetTimeAnimation()
--- Kontiki/front/src/components/pages/Services/Services.jsx
+++ Kontiki/front/src/components/pages/Services/Services.jsx
@@ -5,26 +5,75 @@ import lesFilles from './img/01.jpg';
 import andre from './img/andre.jpg';
 import Rova from './img/Rova.jpg';
 import '../../scss/pages/_services.scss';
+import './Services.js';
 
 function Services() {
   return (
     <div className="carousel">
       <div className="list">
-        <div className="item" style={{ backgroundImage: `url(${progress})` }}>        
+        <div className="item" style={{ backgroundImage: `url(${progress})` }}>
         </div>
 
-        <div className="item" style={{ backgroundImage: `url(${Rova})` }}>        
+        <div className="item" style={{ backgroundImage: `url(${Rova})` }}>
+          <div className="content">
+            <div className="titre">GENERATION DES LEADS</div>
+            {/* <div className="name">Essaye</div> */}
+            <div className="description">
+              Collecter des prospects qualifiés et générez de nouveaux clients.
+            </div>
+            <div className="btns">
+              <button>Voir plus</button>
+            </div>
+          </div>
         </div>
 
-        <div className="item" style={{ backgroundImage: `url(${ensemble})` }}>        
+        <div className="item" style={{ backgroundImage: `url(${ensemble})` }}><div className="content">
+            <div className="titre">GENERATION DES LEADS</div>
+
+            <div className="description">
+              Collecter des prospects qualifiés et générez de nouveaux clients.
+            </div>
+            <div className="btns">
+              <button>Voir plus</button>
+            </div>
+          </div>
         </div>
 
-        <div className="item" style={{ backgroundImage: `url(${lesFilles})` }}>        
+        <div className="item" style={{ backgroundImage: `url(${lesFilles})` }}>
+        <div className="content">
+            <div className="titre">GENERATION DES LEADS</div>
+
+            <div className="description">
+              Collecter des prospects qualifiés et générez de nouveaux clients.
+            </div>
+            <div className="btns">
+              <button>Voir plus</button>
+            </div>
+          </div>
         </div>
 
-        <div className="item" style={{ backgroundImage: `url(${andre})` }}>        
+        <div className="item" style={{ backgroundImage: `url(${andre})` }}>
+        <div className="content">
+            <div className="titre">GENERATION DES LEADS</div>
+            <div className="description">
+              Collecter des prospects qualifiés et générez de nouveaux clients.
+            </div>
+            <div className="btns">
+              <button>Voir plus</button>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div className="fleches">
+        <button className="prev"><img width="20" height="20" src="https://img.icons8.com/flat-round/64/left--v1.png" alt="left--v1" /></button>
+        <button className="next"><img width="20" height="20" src="https://img.icons8.com/flat-round/64/right--v1.png" alt="right--v1" /></button>
+      </div>
+
+      <div className="timeRunning"></div>
+
+
+
     </div>
   );
 }
