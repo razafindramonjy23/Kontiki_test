21cec133294211233ed1668be8aa363f30f5f243
Red_Rabbit <159623002+razafindramonjy23@users.noreply.github.com>
2024-08-14 17:24:34 +0100

    configurer /api/contacts/

--- Kontiki/KontikiServer/Server/TkServer/backend/contact/admin.py
+++ Kontiki/KontikiServer/Server/TkServer/backend/contact/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContatAdmin(admin.ModelAdmin): 
+    list_display = ('nom', 'prenom', 'email', 'message')
