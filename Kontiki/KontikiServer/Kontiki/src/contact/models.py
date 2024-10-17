d022be98666435d498930f28853580df5b9d3366
razafindramonjy23 <razafinramonjytahina@gmail.com>
2024-10-17 11:23:47 +0300

    commit linux

--- Kontiki/KontikiServer/Kontiki/src/contact/models.py
+++ Kontiki/KontikiServer/Kontiki/src/contact/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Contact(models.Model) : 
     nom = models.CharField(max_length=200) 
     prenom = models.CharField(max_length=200) 
